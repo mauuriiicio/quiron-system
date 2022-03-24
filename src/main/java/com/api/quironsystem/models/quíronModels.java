1fdda80e992d6ec8e3e29cc36c8e21e81e1f12c3
mauuriiicio <mauriciomathias592@gmail.com>
2022-03-24 15:11:27 -0300

    projeto_quiron_system

---
+++ src/main/java/com/api/quironsystem/models/quíronModels.java
@@ -0,0 +1,15 @@
+package com.api.quironsystem.models;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table( name = TB_Quiron_system)
+
+
+
+
+public class quíronModels implements Seriazable{
+    private static final long serialVersionVID = 1l
+
+}
---
+++ src/test/java/com/api/quironsystem/QuironSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.quironsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuironSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
